8e6983bccc191a658e1b2f01c3ea61de69c16d59
@noe.tipo <noe.tipo@gmail.com>
2024-03-12 16:59:36 -0500

    config server

--- ms-catalogo/src/main/java/com/example/mscatalogo/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mscatalogo.repository;
-
-import com.example.mscatalogo.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Categoria, Integer> {
-
-}
